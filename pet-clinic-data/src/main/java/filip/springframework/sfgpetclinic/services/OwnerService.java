b5d6c3af853cc8f5e38d892cce11ee560667b8c4
filipHartman <filip.hartman23@gmail.com>
2018-09-25 19:06:06 +0200

    create model services. closes #5

---
+++ pet-clinic-data/src/main/java/filip/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,13 @@
+package filip.springframework.sfgpetclinic.services;
+
+import filip.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/filip/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package filip.springframework.sfgpetclinic.services;
+
+import filip.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
