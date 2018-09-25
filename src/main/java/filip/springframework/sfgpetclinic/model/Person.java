6ed464c6a9f989a74b5268b5efd15efbbb72d8b8
filipHartman <filip.hartman23@gmail.com>
2018-09-25 13:26:18 +0200

    Add Person POJO class

---
+++ src/main/java/filip/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,13 @@
+package filip.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person  {
+    private String firstName;
+    private String lastName;
+
+
+}
