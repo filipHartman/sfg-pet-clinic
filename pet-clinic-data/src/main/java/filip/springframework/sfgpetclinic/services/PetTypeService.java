2281262762f215c8779c815aa3b43118c1a6b279
filipHartman <filip.hartman23@gmail.com>
2018-09-28 17:51:05 +0200

    add PetTypwService class. closes #29

---
+++ pet-clinic-data/src/main/java/filip/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package filip.springframework.sfgpetclinic.services;
+
+import filip.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
