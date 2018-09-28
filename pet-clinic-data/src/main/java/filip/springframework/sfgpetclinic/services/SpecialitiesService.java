1b98b6544634bc9d85feaf0adc852f3147a72473
filipHartman <filip.hartman23@gmail.com>
2018-09-28 19:53:03 +0200

    add Specialities Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/filip/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package filip.springframework.sfgpetclinic.services;
+
+import filip.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
